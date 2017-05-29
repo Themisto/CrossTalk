e0e1e5e6eb8cb2ae297ca10052ea054473e95d21
Sam Olukotun <otsam88@gmail.com>
2017-05-29 11:59:25 -0700

    Add input box to chat panel

---
+++ src/pages/video/InputBox.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <input placeholder="Enter chat here">
+  <button> Send </button>
+</div>
+</template>
+
+
+<script>
+export default {
+  methods: {
+    sendMessage: function (message) {
+      console.log(message)
+    }
+  },
+  components: {
+
+  }
+}
+
+</script>
+
+
+<style>
+
+</style>
