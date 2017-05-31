b0464d06b1931b22ad791c648f21f21c526bdd9a
Sam Olukotun <otsam88@gmail.com>
2017-05-30 19:42:48 -0700

    Wait for video page to mount before capturing local video

--- src/pages/video/Video.vue
+++ src/pages/video/Video.vue
@@ -22,42 +22,34 @@ import TranslationsPanel from './components/TranslationsPanel.vue'
 import TextBox from './components/TextBox.vue'
 
 export default {
+  methods: {
+    captureLocalVideo: function () {
+      console.log('Beginning video capture...')
+      var localVideo;
+      var localVideoStream;
+      var constraints = {audio: true, video: true};
+
+      navigator.mediaDevices.getUserMedia(constraints)
+      .then((stream) => {
+        // console.dir(stream.getAudioTracks());
+        localVideo = document.getElementById('local-video');
+        localVideoStream = URL.createObjectURL(stream);
+        localVideo.src = localVideoStream;
+      })
+      .catch((err) => {
+        console.error('Failed to acquire local video/audio stream.\n', err);
+      })
+    },
+  },
+  mounted: function () {
+    this.captureLocalVideo()
+  },
   components: {
     ChatsPanel,
     TranslationsPanel,
     TextBox
   }
 }
-
-// Video capture/render. Integrate on DOM mount.
-// =============================================
-// var wsc = new WebSocket('ws://localhost:8080/websocket/');
-// var peerConnCfg = {
-//   'iceServers': [
-//     {'url': 'stun:stun.services.mozilla.com'},
-//     {'url': 'stun:stun.l.google.com:19302'}
-//   ]
-// };
-var localVideo;
-var localVideoStream;
-
-let constraints = {audio: true, video: true};
-
-function pageReady() {
-  navigator.mediaDevices.getUserMedia(constraints).then(
-  (stream) => {
-    // console.dir(stream.getAudioTracks());
-    localVideo = document.getElementById('local-video');
-    localVideoStream = URL.createObjectURL(stream);
-    localVideo.src = localVideoStream;
-  }).catch(
-  (err) => {
-    console.error('Failed to acquire local video/audio stream.\n', err);
-  });
-
-};
-
-window.addEventListener("load", pageReady);
 
 </script>
 
