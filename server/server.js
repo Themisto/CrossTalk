5d8c2ad58f7de8f4a54f494efc4d92fb5613f381
mschlesk <markschleske@gmail.com>
2017-05-31 20:07:10 -0600

    (feat) Start signal server from web server entry point.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 var server = require('./routes.js');
+
+// Start signal server
+require('./signal.js');
+
 var port = process.env.PORT || 8000;
 
 server.listen(port, function() {
   console.log(`Server is listening on ${port}`);
 });
+
