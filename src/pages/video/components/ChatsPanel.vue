903fce14fd63ddbf64fa6a76cdcbf7fd8a44bdc6
Anton Bowden <bowden.anton@gmail.com>
2017-06-01 16:29:14 +0100

    (feat) add basic socket.io text chat

--- src/pages/video/components/ChatsPanel.vue
+++ src/pages/video/components/ChatsPanel.vue
@@ -13,7 +13,10 @@
 
 <script>
 import TextBox from './TextBox.vue'
+import io from '../../../../node_modules/socket.io-client/dist/socket.io.js';
+
 export default {
+
   props: {
     messages: {
       default: function () {
@@ -25,24 +28,74 @@ export default {
       }
     }
   },
+
+  // State variables.
+  // ================
   data: function () {
     return {
-      chat: ''
+      socket: null,  // Socket.io connection to signal server, set by startSocketIO().
+      chat: '',
+      verbose: true // On/off flag for log() method
     }
   },
+
+  // Controller methods.
+  // ===================
   methods: {
-    sendMessage: function () {
+
+    // Convenience method for logging debugging messages
+    log: function() {
+      this.verbose && console.log.apply(console, arguments);
+    },
+
+    // Connect to Signal Server and initiate listeners
+    startSocketIO: function() {
+      // Should point to deployed signal server, or http://localhost:8001 for local testing
+      let SignalServerURL = 'http://localhost:8001';
+
+      this.log(`Connecting to signal server at ${SignalServerURL}...`);
+      this.socket = io(SignalServerURL);
+      console.log('startSocketIO: ', this.socket.id);
+    },
+
+
+
+
+    sendMessage: function() {
+      console.log('sendMessage: ', this.socket.id);
+
       var message = {
         id: this.messages.length,
         text: this.chat
       }
 
+      // this.chat = ''
+      // this.messages.push(message)
+
+      var messages = this.messages;
+
+      this.socket.emit('chat message', this.chat);
       this.chat = ''
-      this.messages.push(message)
+
+      this.socket.on('chat message', function() {
+        console.log(messages, message);
+        messages.push(message);
+      });
+
     },
+
+    displayMessage: function() {
+
+
+    }
   },
+
   components: {
     TextBox
+  },
+
+  mounted: function () {
+    this.startSocketIO();
   }
 }
 
