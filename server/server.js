f61aa0befd25d180ed8dd3f7dc2010f125de5a07
TylerCorkill <TylerCorkill@gmail.com>
2017-06-01 10:44:03 -0700

    (fix) server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 var server = require('./routes.js');
+var https = require('https');
+var fs = require('fs');
 
 // Start signal server
 require('./signal.js');
 
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 80;
 
 server.listen(port, function() {
   console.log(`Web server listening on port ${port}`);
 });
 
+var options = {
+  key: fs.readFileSync('/etc/pki/tls/private/localhost.key', 'utf8'),
+  cert: fs.readFileSync('/etc/pki/tls/certs/localhost.crt', 'utf8')
+}
+
+https.createServer(options, server).listen(443);
