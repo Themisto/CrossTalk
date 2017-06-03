740d19b202963ef6cbb6fef8b97a1efffa3da589
mschlesk <markschleske@gmail.com>
2017-06-03 15:17:03 -0600

    (feat) Create new VideoStreams vue component skeleton.

---
+++ src/pages/video/components/VideoStreams.vue
@@ -0,0 +1,52 @@
+<template>
+<div id="videos">
+  <div>
+    <video id="remote-video" autoplay="true"></video>
+    <text-box :message="{text:'Remote'}"></text-box>
+  </div>
+  <div>
+    <video id="local-video" autoplay="true" muted></video>
+    <text-box :message="{text:'Local'}"></text-box>
+  </div>
+</div>
+</template>
+
+
+<script>
+
+export default {
+
+  props: [
+
+  ],
+
+  data: {
+
+  },
+
+  methods: {
+
+  },
+
+  components: {
+
+  },
+
+  mounted: function() {
+
+  },
+
+  beforeDestroy: function() {
+
+  }
+
+}
+
+</script>
+
+
+<style>
+
+
+</style>
+
