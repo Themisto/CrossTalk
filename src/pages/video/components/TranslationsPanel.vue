e208b057b40c1456e194e93f860036373306bca0
Sam Olukotun <otsam88@gmail.com>
2017-06-04 19:34:35 -0700

    Stream live transcript to TranslationsPanel
    Closes #23.

--- src/pages/video/components/TranslationsPanel.vue
+++ src/pages/video/components/TranslationsPanel.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <text-box
-    v-for="message in messages"
+    v-for="message in transcript"
     :message="message"
     :key="message.id"
   ></text-box>
@@ -12,19 +12,64 @@
 <script>
 import TextBox from './TextBox.vue'
 export default {
-  props: {
-    messages: {
-      default: function () {
-        return [
-          {id: 0, text: 'test_message_0'},
-          {id: 1, text: 'test_message_1'},
-          {id: 2, text: 'test_message_2'},
-        ]
-      }
+  data: function () {
+    return {
+      recognition: null,
+      transcript: [],
+      callHasEnded: false,
     }
   },
   methods: {
+    initializeSpeechRecognition: function () {
+      console.log('Starting speech recognition...');
+      var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 
+      this.recognition = new SpeechRecognition();
+      this.recognition.lang = 'en-US';
+      this.recognition.interimResults = false;
+      this.recognition.maxAlternatives = 5;
+      this.recognition.continuous = true;
+
+      this.recognition.onend = () => {
+        if (this.callHasEnded) {
+          console.log('Stopped speech recognition.');
+        } else {
+          this.recognition.start();
+        }
+      };
+
+      this.recognition.onresult = (event) => {
+        var latest = event.results.length - 1;
+        var result = event.results[latest][0].transcript
+        var timestamp = new Date(Date.now());
+        timestamp = timestamp.toLocaleTimeString();
+        this.transcript.push({id: timestamp, text: result});
+      };
+      
+      this.recognition.start();
+    },
+    
+    listen: function () {
+      if (this.callHasEnded) {
+        this.recognition.stop();
+      } else {
+        this.initializeSpeechRecognition();
+      }
+    },
+    
+    toggleListen: function () {
+      this.callHasEnded = !this.callHasEnded;
+      this.listen();
+    },
+  },
+  mounted: function () {
+    this.listen();
+  },
+  beforeDestroy: function () {
+    if (this.recognition) {
+      this.callHasEnded = true;
+      this.recognition.stop();
+    }
   },
   components: {
     TextBox
