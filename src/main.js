58eebe5e5d0c5280b0f4e9f894c93595eea859b8
Anton Bowden <bowden.anton@gmail.com>
2017-06-09 02:15:54 +0100

    (feat) Start building user profile page

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import VueRouter from 'vue-router';
 import Home from './pages/home/Home.vue';
 import TodoList from './pages/list/TodoList.vue';
 import Video from './pages/video/Video.vue';
+import Profile from './pages/profile/Profile.vue';
 import Login from './pages/login/Login.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/profile', component: Profile },
   { path: '/list', component: TodoList },
   { path: '/video', component: Video },
   { path: '/room/:id', component: Video },
---
+++ src/pages/profile/Profile.vue
@@ -0,0 +1,10 @@
+<template>
+<div id="profile-page">
+  <h1>Profile</h1>
+</div>
+
+</template>
+
+<script></script>
+
+<style></style>
