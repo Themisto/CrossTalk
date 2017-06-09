d4a97ba4eefa64586035efe0e8fb5a0a3d359030
mschlesk <markschleske@gmail.com>
2017-06-08 18:23:14 -0600

    (feat) Build FriendsList component skeleton.

---
+++ src/FriendsList.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+Friends
+</div>
+</template>
+
+
+
+<script>
+
+export default {
+
+  props: [],
+  data: function() {
+    return {};
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+
+}
+
+</script>
+
+
+
+<style scoped>
+
+</style>
