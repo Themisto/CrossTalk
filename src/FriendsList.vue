7b7487e9921dbcd371b30343803b12e7fbcf4829
mschlesk <markschleske@gmail.com>
2017-06-09 18:00:17 -0600

    (feat) Add dropdown contents with dummy data.

--- src/FriendsList.vue
+++ src/FriendsList.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="friends-list">
-
+  <div v-for="friend in friends">{{friend.name}}</div>
 </div>
 </template>
 
@@ -12,7 +12,14 @@ export default {
 
   props: [],
   data: function() {
-    return {};
+    return {
+      friends: [
+        {name: 'Anton'},
+        {name: 'Mark'},
+        {name: 'Sam'},
+        {name: 'Tyler'}
+      ]
+    };
   },
   methods: {
 
@@ -30,7 +37,15 @@ export default {
 <style scoped>
 
 #friends-list {
-
+  background-color: black;
+  position: absolute;
+  display: block;
+  top: 54px;
+  left: 0px;
+  min-width: 300px;
+  z-index: 100;
+  /*Opacity doesn't seem to override Navbar styling*/
+  opacity: 1;
 }
 
 </style>
