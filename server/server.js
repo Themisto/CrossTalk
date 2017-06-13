8f818293a05b80151a5e62cc960677885ec0b892
mschlesk <markschleske@gmail.com>
2017-06-13 03:36:07 -0600

    (refactor) Move signal server routes into new signalRoutes.js file.

--- server/server.js
+++ server/server.js
@@ -4,77 +4,23 @@ var db = require('../database/db.js');
 var https = require('https');
 var http = require('http');
 var fs = require('fs');
-var utils = require('./utils.js');
-var User = require('../database/models/user.js');
+var signal = require('./signalRoutes.js');
 
 // For deployment use port 80
-// var port = process.env.PORT || 80;
 var port = process.env.PORT || 8000;
+var server;
 
-// More deployment stuff, for https connections use port 443 and credentials
-// var options = {
-//   key: fs.readFileSync('/etc/pki/tls/private/localhost.key', 'utf8'),
-//   cert: fs.readFileSync('/etc/pki/tls/certs/localhost.crt', 'utf8')
-// }
+if (port === 80) {
+  // More deployment stuff, for https connections use port 443 and credentials
+  var options = {
+    key: fs.readFileSync('/etc/pki/tls/private/localhost.key', 'utf8'),
+    cert: fs.readFileSync('/etc/pki/tls/certs/localhost.crt', 'utf8')
+  };
+  server = https.createServer(options, server).listen(443);
+} else {
+  server = http.createServer(server).listen(port);
+}
 
-// var secureServer = https.createServer(options, server).listen(443);
-// var io = require("socket.io").listen(secureServer);
-
-
-var server = http.createServer(server).listen(port);
-
-var io = require("socket.io").listen(server);
-
-
+var io = require('socket.io').listen(server);
 console.log(`Signal server listening on port ${port}`);
-
-
-io.on('connection', function(socket) {
-  console.log(`Connection received from socket "${socket.id}"`);
-
-  // User attempting to join or create a room
-  socket.on('enter room', function(room) {
-    console.log(`Socket "${socket.id}" requested to join room "${room}"`);
-
-    // Add user to room
-    socket.join(room);
-
-    // Count number of users in room
-    io.in(room).clients((err, clients) => {
-      if (clients.length === 1) { // if previous empty room
-        socket.emit('created room', room);
-      } else {  // if occupied room
-        socket.emit('joined room', room);
-        socket.to(room).emit('callee joined', room);
-      }
-    });
-
-  });
-
-  // User attempting to send WebRTC (ICECandidates, session descriptions) data to other user
-  socket.on('relay', function(data) {
-    console.log(`Relaying "${data.message.type}" message from socket "${socket.id}" to room "${data.room}"`);
-    socket.to(data.room).emit('relay', data.message);
-  });
-
-  // User attempt to send text chat message to other user
-  socket.on('message', function(data) {
-    console.log(`Relaying chat message from socket "${socket.id}" to room "${data.room}"`);
-    socket.to(data.room).emit('message', data.message);
-  });
-
-  // User attempt to send text chat message to other user
-  socket.on('translateText', function(data) {
-    console.log(`Relaying chat translateText from socket "${socket.id}" to room "${data.room}"`);
-    socket.to(data.room).emit('translateText', data.message);
-  });
-
-  socket.on('metric', function(data) {
-    console.log(`"${data.type}" type metrics data received from client`);
-    let userID = utils.idFromToken(data.idToken);
-    console.log(`  JWT: ${userID}\n  Duration: ${data.sessionData.duration}\n  fromLang: ${data.sessionData.fromLang}\n  toLang: ${data.sessionData.toLang}\n`);
-    // Store data in database
-    User.updateCallMetricsById(userID, data.sessionData);
-  });
-
-});
+signal(io);
---
+++ server/signalRoutes.js
@@ -0,0 +1,17 @@
+var handlers = require('./signalHandlers.js');
+
+module.exports = function(io) {
+
+  io.on('connection', function(socket) {
+
+    console.log(`Connection received from socket "${socket.id}"`);
+
+    socket.on('enter room', message => handlers.enterRoom(message, socket, io));
+    socket.on('relay', message => handlers.relay(message, socket, io));
+    socket.on('message', message => handlers.message(message, socket, io));
+    socket.on('translateText', message => handlers.translateText(message, socket, io));
+    socket.on('metric', message => handlers.metric(message, socket, io));
+
+  });
+
+};
