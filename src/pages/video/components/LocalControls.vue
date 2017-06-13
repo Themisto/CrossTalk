4e4f252c56d7c09e661aa7ae16c41bc17f8e7b1e
TylerCorkill <TylerCorkill@gmail.com>
2017-06-13 15:31:56 -0700

    (fix) Add new files

---
+++ src/pages/video/components/LocalControls.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  Local
+</div>
+</template>
+
+
+
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  }
+}
+
+</script>
+
+
+
+<style scoped>
+  div {
+    position: absolute;
+    display: flex;
+    margin-top: -30px;
+  }
+</style>
