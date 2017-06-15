0cdc1b0ca359ecb80e277467072d80d0e2581d9c
mschlesk <markschleske@gmail.com>
2017-06-14 22:30:49 -0600

    (feat) Display and vote on/update the remote user's rating.

--- src/pages/video/components/RemoteControls.vue
+++ src/pages/video/components/RemoteControls.vue
@@ -5,12 +5,10 @@
     </div>
     <div id="votes">
       <div>
-        <!-- <p id="upvote" v-on:click="handleUpvote">&#9757</p> -->
-        <p id="upvote">&#9757</p>
-        <!-- <p id="downvote" v-on:click="handleDownvote">&#9759</p> -->
-        <p id="downvote">&#9759</p>
+        <p id="upvote" v-on:click="handleVote('up')">&#9757</p>
+        <p id="downvote" v-on:click="handleVote('down')">&#9759</p>
       </div>
-      <p>1234</p>
+      <p v-if="this.partnerRating !== null">{{this.partnerRating}}</p>
     </div>
   </div>
 </template>
@@ -33,9 +31,15 @@
         addFriendButton: null,
         upvoteButton: null,
         downvoteButton: null,
-        partnerId: null,
         publicId: null,
-        friendAdded: false
+        rating: null,
+        partnerId: null,
+        partnerRating: null,
+        friendAdded: false,
+        votes: {
+          up: false,
+          down: false
+        }
       }
     },
     watch: {
@@ -43,7 +47,7 @@
         this.registerListeners();
       },
       inCall: function() {
-        this.inCall && this.sendPublicId();
+        this.inCall && this.sendPublicData();
       }
     },
     methods: {
@@ -54,9 +58,10 @@
 
       registerListeners: function() {
         this.socket.on('relay', (data) => {
-          if (data.type === 'publicId') {
-            this.log(`Received partner's public id "${data.publicId}"`);
+          if (data.type === 'publicData') {
+            this.log(`Received partner's public data`);
             this.partnerId = data.publicId;
+            this.partnerRating = data.rating;
           }
         });
 
@@ -64,37 +69,49 @@
       },
 
       getPublicId: function() {
+        this.log(`Attempting to fetch current user's public id...`);
         axios.get('/api/users/publicId', {
           headers: {'x-access-token': `Bearer ${localStorage.id_token}`}
         })
         .then(response => {
           this.publicId = response.data;
+          if (this.inCall) { this.sendPublicData(); }
         })
         .catch(err => console.error('Failure retrieving public id:', err));
       },
 
-      sendPublicId: function() {
-        this.log('Sending public id');
-        if (this.publicId) {
+      getRating: function() {
+        this.log(`Attempting to fetch current user's rating...`);
+        axios.get('/api/users/rating', {
+          headers: {'x-access-token': `Bearer ${localStorage.id_token}`}
+        })
+        .then(response => {
+          this.rating = response.data;
+          if (this.inCall) { this.sendPublicData(); }
+        })
+        .catch(err => console.error('Failure retrieving public id:', err));
+      },
+
+      sendPublicData: function() {
+        if (this.publicId !== null && this.rating !== null) {
+          this.log('Sending public data...');
           this.socket.relay({
-            type: 'publicId',
-            publicId: this.publicId
+            type: 'publicData',
+            publicId: this.publicId,
+            rating: this.rating.net
           });
         }
       },
 
       handleAddFriend: function() {
-        this.log('Attempting to add friend...');
         if (this.partnerId && !this.friendAdded) {
+          this.log('Attempting to add friend...');
           this.friendAdded = true;  // Set boolean so user can't spam the button
           this.addFriendButton.setAttribute('background-color', 'grey');
-          axios.post('/api/users/friends',
-            { friendID: this.partnerId },
-            {
-              headers: {
-                'x-access-token': `Bearer ${localStorage.id_token}`
-              }
-            }
+          axios.post(
+            '/api/users/friends',
+            { friendId: this.partnerId },
+            { headers: {'x-access-token': `Bearer ${localStorage.id_token}`} }
           )
           .then(response => this.log('Successfully added friend'))
           .catch(err => {
@@ -103,11 +120,37 @@
             this.friendAdded = false;
           });
         }
+      },
+
+      handleVote: function(direction) {
+        if (this.partnerId) {
+          this.log(`Attempting to update partner's rating...`);
+          this.votes[direction] = !this.votes[direction];
+          let votesData = {
+            partnerId: this.partnerId,
+            votes: {
+              up: direction === 'up' ? (this.votes.up ? 1 : -1) : 0,
+              down: direction === 'down' ? (this.votes.down ? 1 : -1) : 0
+            }
+          };
+          this.partnerRating += votesData.votes.up - votesData.votes.down;
+          this.partnerRating
+          axios.put(
+            '/api/users/rating',
+            votesData,
+            { headers: {'x-access-token': `Bearer ${localStorage.id_token}`} }
+          )
+          .then(response => this.log('Successfully updated user rating'))
+          .catch(err => {
+            console.err('Failed to update rating:', err);
+          });
+        }
       }
     },
     mounted: function() {
       this.addFriendButton = document.getElementById('addFriend');
       this.getPublicId();
+      this.getRating();
     }
   }
 
