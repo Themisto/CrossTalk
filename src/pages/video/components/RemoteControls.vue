7cdcd7808ab5c45368f8e36ecdda121fe404265f
mschlesk <markschleske@gmail.com>
2017-06-14 18:58:21 -0600

    (feat) Send and receive public id strings between callers video participants.

--- src/pages/video/components/RemoteControls.vue
+++ src/pages/video/components/RemoteControls.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="controls">
-    <div id="add">
+    <div id="add" v-on:click="handleAddFriend">
       <p>+</p>
     </div>
     <div id="votes">
       <div>
-        <p>&#9757</p>
-        <p>&#9759</p>
+        <p id="upvote" v-on:click="handleUpvote">&#9757</p>
+        <p id="downvote" v-on:click="handleDownvote">&#9759</p>
       </div>
       <p>1234</p>
     </div>
@@ -17,10 +17,58 @@
 
 <script>
 
+  import axios from 'axios';
+
   export default {
-    data() {
+    props: [
+      'socket',
+      'socketReady',
+      'roomJoined'
+    ],
+    data: function() {
       return {
+        partnerId: null,
+        publicId: null
       }
+    },
+    watch: {
+      socketReady: this.registerListeners,
+      roomJoined: function() {
+
+      }
+    },
+    methods: {
+      registerListeners: function() {
+        this.socket.on('relay', (data) => {
+          if (data.type === 'publicId') {
+            this.partnerId = data.publicId;
+          }
+        });
+      },
+
+      getPublicId: function() {
+        axios.get('/api/users/publicId', {
+          headers: {'x-access-token': `Bearer ${localStorage.id_token}`}
+        })
+        .then(response => {
+          this.publicId = response.data;
+        })
+        .catch(err => {
+          console.error('Failure retrieving public id:', err);
+        });
+      },
+
+      sendPublicId: function() {
+        if (this.publicId) {
+          this.socket.relay({
+            type: 'publicId',
+            publicId: this.id_public
+          });
+        }
+      }
+    },
+    mounted: function() {
+      this.getPublicId();
     }
   }
 
