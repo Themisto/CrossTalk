49423a5ae4a0322470da1b3209e310a35e146c66
TylerCorkill <TylerCorkill@gmail.com>
2017-06-16 10:15:13 -0700

    (fix) Dropdown style

--- src/pages/home/components/Banner.vue
+++ src/pages/home/components/Banner.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home-banner">
-    <img src="../img/banner.png">
+    <!-- <img src="../img/banner.png"> -->
+    <h1>CrossTalk</h1>
   </div>
 </template>
 
@@ -23,15 +24,22 @@
 
 <style>
  .home-banner {
-    height: 400px;
+    height: 100px;
     width: 100vw;
     overflow: hidden;
     align-items: center;
-    background: rgba(0,0,0,0.6);
+    background: rgba(0,0,0,0.4);
   }
 
   .home-banner img {
     margin: 20px;
     height: calc(100% - 40px);
+  }
+
+  .home-banner h1 {
+    margin: 20px;
+    color: white;
+    height: calc(100% - 40px);
+    letter-spacing: 4px;
   }
 </style>
